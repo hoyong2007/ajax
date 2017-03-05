e848d95f294ea8ce7f361858c759f99a5d26de14
hoyong2007 <hoyong2007@naver.com>
2017-03-05 09:11:54 -0800

    first

---
+++ web/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+		url(r'^$', views.index, name='index'),
+		url(r'^anounce/anounce.html',views.anounce, name="anounce"),
+		url(r'^anounce/get_list',views.get_list, name="get_list"),
+		url(r'^anounce/detail_list/(?P<num>[0-9]+)$', views.detail_list, name="detail_list"),
+		url(r'^anounce/get_context/(?P<num>[0-9]+)$', views.get_context, name="get_context"),
+]
